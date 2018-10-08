fb8a06c3675057e6719a67b2f360a350bdf6b76f
Michael Gangolf <migamiga@web.de>
2018-10-08 13:33:03 +0200

    layout

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,25 +1,38 @@
-var Activity = require('android.app.Activity');
-var activity = new Activity(Ti.Android.currentActivity);
-var ParkedTextView = require("com.goka.parkedtextview.ParkedTextView");
-var TextWatcher = require("android.text.TextWatcher");
-var parkedView = new ParkedTextView(activity);
+// hyperloop classes
+const Activity = require("android.app.Activity");
+const LayoutParams = require("android.widget.FrameLayout.LayoutParams");
+const ViewGroupLayoutParams = require("android.view.ViewGroup.LayoutParams");
+const Gravity = require("android.view.Gravity");
+const ParkedTextView = require("com.goka.parkedtextview.ParkedTextView");
+const TextWatcher = require("android.text.TextWatcher");
+const activity = new Activity(Ti.Android.currentActivity);
+const parkedView = new ParkedTextView(activity);
 
+// define parkedTextView options
 parkedView.setParkedTextColor("#000000");
 parkedView.setParkedHintColor("#999999");
 parkedView.setParkedText(".slack.com");
 parkedView.setHintText("your-team");
 
-var textWatcher = new TextWatcher({
+// layout parkedTextView
+const layoutParams = new LayoutParams(ViewGroupLayoutParams.WRAP_CONTENT, ViewGroupLayoutParams.WRAP_CONTENT, Gravity.CENTER);
+parkedView.setLayoutParams(layoutParams);
+
+// add event watcher
+const textWatcher = new TextWatcher({
 	afterTextChanged: afterTextChanged,
 });
-
-
 function afterTextChanged(s) {
 	console.log("After: " + s + " " + parkedView.getTypedText());
 }
 parkedView.addTextChangedListener(textWatcher);
+
+// add view to app
 $.view_lbl.add(parkedView);
-$.btn_clear.addEventListener("click",function(e){
+
+// clear text button
+function clearText(){
 	parkedView.setEmptyText();
-})
+}
+
 $.index.open();
